0da671a5c6dc57dee0931ecef07c05b62ca13689
rakshanayak01 <96346873+rakshanayak01@users.noreply.github.com>
2023-01-08 22:52:08 +0530

    Create Chocalates.java

---
+++ Chocalates.java
@@ -0,0 +1,19 @@
+package tech1;
+import java.util.Scanner;
+public class Chocolates {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a[] = new int[n];
+		int index1 = 0;
+		for(int i = 0; i < n; i++) {
+			int in = sc.nextInt();
+			if(in != 0)
+				a[index1++] = in;
+		}
+		for(int i = 0; i < n; i++)
+			System.out.print(a[i] + " ");
+		System.out.println();
+	}
+}
